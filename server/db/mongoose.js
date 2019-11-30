5bb2a730a6d4aaaa796c687e89ca55f057d553cb
1anhthanh1997 <3anhthanh1997@gmail.com>
2019-12-01 00:18:11 +0700

    deploy

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URL,{
+mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
