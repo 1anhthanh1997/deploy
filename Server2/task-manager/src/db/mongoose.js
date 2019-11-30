6da88ba4e361170fda9577938bd91be243e10ffe
1anhthanh1997 <3anhthanh1997@gmail.com>
2019-11-30 18:35:16 +0700

    deploy 2

--- Server2/task-manager/src/db/mongoose.js
+++ Server2/task-manager/src/db/mongoose.js
@@ -1,10 +1,16 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/onlineTeaching',{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
+let DB_CONECTION= 'mongodb+srv://root:anhthanh1997@cluster0-umvy9.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true },
+    () => { console.log('connected to db') }
+)
+// mongoose.connect('mongodb://localhost:27017/onlineTeaching',{
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// });
 
 module.exports = {mongoose};
